cadfca0cb9b49aaf1c061ab9045215d69039be95
king5ula <davidtu@missionreadyhq.com>
2026-07-16 14:42:39 +1200

    updated frontend url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://my-portfolio-frontend-domn8r.vercel.app'
+        'my-portfolio-frontend-kg5dpdn5v-domn8r.vercel.app'
     ],
     methods: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
